90ed9f2f4a2d14d6dc945a8147205dba269790e8
Atle Frenvik Sveen <atle.sveen@gmail.com>
2012-12-17 20:51:57 +0100

    scaffolding added

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,9 +3,9 @@ var blog = {};
 (function(ns){
 
     ns.createMap = function(){
-        var map = L.map('map', {zoomControl: false}).setView([60, 10], 5);
+        var map = L.map('map', {zoomControl: false}).setView([60, 10], 10);
         map.attributionControl.setPrefix("");
-        L.tileLayer('http://{s}.tile.cloudmade.com/{key}/999/256/{z}/{x}/{y}.png',
+        L.tileLayer('http://{s}.tile.cloudmade.com/{key}/998/256/{z}/{x}/{y}.png',
             {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
                 maxZoom: 18,
@@ -13,9 +13,11 @@ var blog = {};
 
             }
         ).addTo(map);
-    }
+    };
 
-
+    window.onscroll = function() {
+        $("#map").css("top", window.pageYOffset + "px");
+    };
 
 }(blog));
 
